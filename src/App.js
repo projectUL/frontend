7469c438b43e4400361a4e945e0693e462fee407
FollowerOfRicardo <oazaa2018@gmail.com>
2022-05-20 16:14:45 +0200

    change style homePage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 
+import { useContext } from "react";
+
 import MainNavigation from "./components/layout/MainNavigation";
 import HomePage from "./pages/HomePage";
 import OffersPage from "./pages/OffersPage";
@@ -13,8 +15,11 @@ import ProfilePage from "./pages/ProfilePage";
 import EditProfilePage from "./pages/EditProfilePage";
 import OfferPage from "./pages/OfferPage";
 import NotFound from "./pages/NotFound";
+import AuthContext from "./components/context/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <div>
       <MainNavigation />
@@ -26,8 +31,12 @@ function App() {
         <Route path="companies/:id" element={<CompanyPage />} />
         <Route path="register" element={<AuthPage register={true} />} />
         <Route path="login" element={<AuthPage login={true} />} />
-        <Route path="profile" element={<EditProfilePage />} />
-        <Route path="profile/:id" element={<ProfilePage />} />
+        {authCtx.accessLevel !== 0 && (
+          <>
+            <Route path="profile" element={<EditProfilePage />} />
+            <Route path="profile/:id" element={<ProfilePage />} />
+          </>
+        )}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
