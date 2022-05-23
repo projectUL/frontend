09c16b91252b326807f896b9d8a2a1031e326142
FollowerOfRicardo <oazaa2018@gmail.com>
2022-05-23 23:14:16 +0200

    Create component ProjectsPage

--- src/pages/Account/Profile/ProjectsPage.js
+++ src/pages/Account/Profile/ProjectsPage.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
+import classes from "./ProjectsPage.module.css";
+
+import { FaPlus } from "react-icons/fa";
+import Project from "../../../components/profile/Project";
+import AddProject from "../../../components/profile/AddProject";
 
 function ProjectsPage() {
+  const [addProjectDisplay, setAddProjectDisplay] = useState(false);
   return (
-    <>
-      <h1>Projects</h1>
-    </>
+    <div className={classes.projects_wrap}>
+      <div className={classes.addProjects}>
+        <FaPlus onClick={() => setAddProjectDisplay(true)} />
+      </div>
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      {addProjectDisplay && <AddProject setAddProjectDisplay={setAddProjectDisplay} />}
+    </div>
   );
 }
 
