d984c873aa0553894201fba14851e7388034a8ec
FollowerOfRicardo <oazaa2018@gmail.com>
2022-05-23 23:11:35 +0200

    Add component ProjectViewProfile

---
+++ src/components/profile/ProjectViewProfile.js
@@ -0,0 +1,15 @@
+import classes from "./ProjectViewProfile.module.css";
+import git from "./../../../src/images/git.jpg";
+
+const ProjectViewProfile = ({ onClick }) => {
+  return (
+    <div className={classes.project_wrap} onClick={() => onClick()}>
+      <img src={git} alt="git" />
+      <div className={classes.title_wrap}>
+        <p className={classes.title}>PornHub</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectViewProfile;
