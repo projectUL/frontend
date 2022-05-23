39d6fd7597d5a61deacf0a7fce9f9ef9ffb3fc15
FollowerOfRicardo <oazaa2018@gmail.com>
2022-05-23 23:11:00 +0200

    Add component Project

---
+++ src/components/profile/Project.js
@@ -0,0 +1,18 @@
+import classes from "./Project.module.css";
+import CloseButton from "./../profile/CloseButton";
+import git from "./../../../src/images/git.jpg";
+const Project = () => {
+  return (
+    <div className={classes.project_wrap}>
+      <img src={git} alt="git" />
+      <div className={classes.title_wrap}>
+        <p className={classes.title}>PornHub</p>
+      </div>
+      <div className={classes.project}>
+        <CloseButton />
+      </div>
+    </div>
+  );
+};
+
+export default Project;
